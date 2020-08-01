3c9444661bae816a90dad30962e34e5230c47b0f
jwwwb <2066729323@qq.com>
2020-08-01 23:41:55 +0800

    test

--- scripts/install-unx.sh
+++ scripts/install-unx.sh
@@ -23,8 +23,9 @@ echo "---------install pkgs!!!!!!----------"
 #$TEXDIR/bin/*/tlmgr install $(download https://yihui.org/gh/tinytex/tools/pkgs-custom.txt | tr '\n' ' ')
 #download https://yihui.org/gh/tinytex/tools/pkgs-custom.txt | tr '\n' ' ' | $TEXDIR/bin/*/tlmgr install
 #$TEXDIR/bin/*/tlmgr install $(tr $GITHUB_WORKSPACE/scripts/pkgs-custom.txt '\n' ' ')
-cat $GITHUB_WORKSPACE/scripts/pkgs-custom.txt | tr '\n' ' '
-cat $GITHUB_WORKSPACE/scripts/pkgs-custom.txt | tr '\n' ' ' | $TEXDIR/bin/*/tlmgr install
+#cat $GITHUB_WORKSPACE/scripts/pkgs-custom.txt | tr '\n' ' '
+#cat $GITHUB_WORKSPACE/scripts/pkgs-custom.txt | tr '\n' ' ' | $TEXDIR/bin/*/tlmgr install
+$TEXDIR/bin/*/tlmgr install amsfonts amsmath atbegshi atveryend auxhook bibtex bigintcalc bitset booktabs dvips ec epstopdf-pkg etexcmds etoolbox euenc fancyvrb filehook float fontspec framed geometry gettitlestring grffile helvetic hycolor hyperref iftex inconsolata infwarerr intcalc kvdefinekeys kvoptions kvsetkeys latex-amsmath-dev latex-tools-dev latexmk letltxmacro lm-math ltxcmds lualatex-math mdwtools metafont mfware natbib pdfescape pdftexcmds refcount rerunfilecheck stringenc tex times tipa tools unicode-math uniquecounter url xcolor xkeyval xunicode zapfding unicode-math filehook xecjk xltxtra realscripts fancyhdr lastpage ctex ms cjk ulem environ trimspaces zhnumber collection-fontsrecommended
 #$TEXDIR/bin/*/tlmgr install $(cat $GITHUB_WORKSPACE/scripts/pkgs-custom.txt | tr '\n' ' ')
 
 if [ "$1" = '--admin' ]; then
