9ff6c75806d0bdc9028a561b4b21c30688c90215
jwwwb <2066729323@qq.com>
2020-08-04 11:06:07 +0800

    dd

--- scripts/md-pdf.sh
+++ scripts/md-pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 shopt -s expand_aliases
-sudo apt-get install pandoc                 #安装pandoc
+sudo apt-get install pandoc #安装pandoc
 
 #安装TinyTex
 #设置安装路径，如果TMPDIR为空值，则等价于cd /tmp
@@ -8,11 +8,11 @@ cd ${TMPDIR:-/tmp}
 
 #根据操作系统来设置安装路径和安装命令
 if [ $(uname) = 'Darwin' ]; then
-  TEXDIR=${TINYTEX_DIR:-~/Library/TinyTeX}
-  alias download='curl -sL'
+    TEXDIR=${TINYTEX_DIR:-~/Library/TinyTeX}
+    alias download='curl -sL'
 else
-  TEXDIR=${TINYTEX_DIR:-~/.TinyTeX}
-  alias download='wget -qO-'
+    TEXDIR=${TINYTEX_DIR:-~/.TinyTeX}
+    alias download='wget -qO-'
 fi
 
 #在当前目录下载install-tl-unx.tar.gz和tinytex.profile两个文件
@@ -22,28 +22,28 @@ TLREPO=${CTAN_REPO:-http://mirrors.ibiblio.org/CTAN/systems/texlive/tlnet}
 TLURL="${TLREPO}/install-tl-unx.tar.gz"
 cp $GITHUB_WORKSPACE/scripts/tinytex.profile ./
 if [ $(uname) = 'Darwin' ]; then
-  curl -LO $TLURL
+    curl -LO $TLURL
 else
-  wget $TLURL
-  # ask `tlmgr path add` to add binaries to ~/bin instead of the default
-  # /usr/local/bin unless this script is invoked with the argument '--admin'
-  # (e.g., users want to make LaTeX binaries available system-wide), in which
-  # case we personalize texmf variables
-  if [ "$1" = '--admin' ]; then
-    echo 'TEXMFCONFIG $HOME/.TinyTeX/texmf-config' >> tinytex.profile
-    echo 'TEXMFHOME $HOME/.TinyTeX/texmf-home' >> tinytex.profile
-    echo 'TEXMFVAR $HOME/.TinyTeX/texmf-var' >> tinytex.profile
-  else
-    mkdir -p $HOME/bin
-    echo "tlpdbopt_sys_bin ${HOME}/bin" >> tinytex.profile
-  fi
+    wget $TLURL
+    # ask `tlmgr path add` to add binaries to ~/bin instead of the default
+    # /usr/local/bin unless this script is invoked with the argument '--admin'
+    # (e.g., users want to make LaTeX binaries available system-wide), in which
+    # case we personalize texmf variables
+    if [ "$1" = '--admin' ]; then
+        echo 'TEXMFCONFIG $HOME/.TinyTeX/texmf-config' >>tinytex.profile
+        echo 'TEXMFHOME $HOME/.TinyTeX/texmf-home' >>tinytex.profile
+        echo 'TEXMFVAR $HOME/.TinyTeX/texmf-var' >>tinytex.profile
+    else
+        mkdir -p $HOME/bin
+        echo "tlpdbopt_sys_bin ${HOME}/bin" >>tinytex.profile
+    fi
 fi
 
 # no need to personalize texmf variables if not installed by admin
 if [ "$1" != '--admin' ]; then
-  echo 'TEXMFCONFIG $TEXMFSYSCONFIG' >> tinytex.profile
-  echo 'TEXMFHOME $TEXMFLOCAL' >> tinytex.profile
-  echo 'TEXMFVAR $TEXMFSYSVAR' >> tinytex.profile
+    echo 'TEXMFCONFIG $TEXMFSYSCONFIG' >>tinytex.profile
+    echo 'TEXMFHOME $TEXMFLOCAL' >>tinytex.profile
+    echo 'TEXMFVAR $TEXMFSYSVAR' >>tinytex.profile
 fi
 
 tar -xzf install-tl-unx.tar.gz
@@ -59,12 +59,12 @@ alias tlmgr='./bin/*/tlmgr'
 tlmgr option repository "$TLREPO"
 
 if [ "$3" != '' ]; then
-  tlmgr option repository "$3"
-  if [ "$4" != '' ]; then
-    tlmgr --repository http://www.preining.info/tlgpg/ install tlgpg
-  fi
-  # test if the repository is accessible; if not, set the default CTAN repo
-  tlmgr update --list || ./tlmgr option repository ctan
+    tlmgr option repository "$3"
+    if [ "$4" != '' ]; then
+        tlmgr --repository http://www.preining.info/tlgpg/ install tlgpg
+    fi
+    # test if the repository is accessible; if not, set the default CTAN repo
+    tlmgr update --list || ./tlmgr option repository ctan
 fi
 tlmgr install latex-bin luatex xetex
 
@@ -83,14 +83,14 @@ $TEXDIR/bin/*/tlmgr install amsfonts amsmath atbegshi atveryend auxhook bibtex b
 
 #根据操作系统来添加path，虽然现在貌似是不work的状态
 if [ "$1" = '--admin' ]; then
-  if [ "$2" != '--no-path' ]; then
-    sudo $TEXDIR/bin/*/tlmgr path add
-  fi
+    if [ "$2" != '--no-path' ]; then
+        sudo $TEXDIR/bin/*/tlmgr path add
+    fi
 else
-  $TEXDIR/bin/*/tlmgr path add || true
+    $TEXDIR/bin/*/tlmgr path add || true
 fi
 
-export PATH=$PATH:$HOME/bin                 #将Tex相关可执行文件添加到PATH
+export PATH=$PATH:$HOME/bin #将Tex相关可执行文件添加到PATH
 
 #遍历repo下的文件，将所有md文件转换到$GITHUB_WORKSPACE/PDFs路径下变成pdf文件
 DOC_PATH=$GITHUB_WORKSPACE/docs
